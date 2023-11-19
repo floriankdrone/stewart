75461cf1a0a1e4e6b64ddb447239650e066f5f76
FD <florian.kdrone@gmail.com>
2023-11-19 15:17:35 +0100

    add: todos and nexts tasks

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # stewart
+
+## TODO
+
+- [x] Add docker file
+- [ ] Add Unit tests
+- [ ] Add Controller tests
+- [ ] Add E2E test
+- [ ] Add eslint rules
--- src/model.js
+++ src/model.js
@@ -24,13 +24,13 @@ class Auth {
   }
 
   /**
-   *
+   * Find Auth object according to its email
    * @param {Object} db
    * @param {String} email
    * @returns Auth
    */
   static async findByEmail(db, email) {
-    const { email, password } = await db.result(
+    const { password } = await db.result(
       "SELECT * FROM auth WHERE email=$1 LIMIT 1",
       [email]
     );
@@ -38,17 +38,33 @@ class Auth {
   }
 
   /**
+   * TODO: implement function
+   * @returns Boolean
+   */
+  hasValidPassword() {
+    return !!this.password;
+  }
+
+  /**
+   * TODO: implement function
+   * @returns Boolean
+   */
+  hasValidEmail() {
+    return !!this.email;
+  }
+
+  /**
    * Check if auth object valid
-   * @returns [Boolean]
+   * @returns Boolean
    */
   async isValid() {
-    return !!this.email && !!this.password;
+    return this.hasValidEmail() && this.hasValidPassword();
   }
 
   /**
    * Saves the auth object
-   * @todo handle duplicate key value error
-   * @todo handle auth not valid
+   * TODO: handle duplicate key value error
+   * TODO: handle auth not valid
    */
   async save() {
     if (!this.isValid()) throw new Error("Auth not valid");
